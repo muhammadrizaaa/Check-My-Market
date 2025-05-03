7d21045e91e242a26f6c70c516b45b8b0dd47a4d
Muhammad Riza Rabbani <muhammad_riza_29rpl@student.smktelkom-mlg.sch.id>
2025-05-03 18:30:55 +0700

    Penambahan List (statis) di HomeScreen.kt

--- app/src/main/java/com/riza0004/checkmymarket/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/riza0004/checkmymarket/ui/screen/HomeScreen.kt
@@ -1,21 +1,48 @@
 package com.riza0004.checkmymarket.ui.screen
 
+import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.riza0004.checkmymarket.R
+import com.riza0004.checkmymarket.ui.theme.CheckMyMarketTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainHomeScreen(){
+    var expanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(
@@ -27,16 +54,126 @@ fun MainHomeScreen(){
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-                )
+                ),
+                actions = {
+                    IconButton(
+                        onClick = {expanded = !expanded}
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_more_vert_24),
+                            contentDescription = stringResource(R.string.more)
+                        )
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(stringResource(R.string.add_product))
+                                },
+                                onClick = {
+                                    expanded = false
+                                    Toast.makeText(
+                                        context,
+                                        "Add Product",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            )
+                            DropdownMenuItem(
+                                text = {
+                                    Text(stringResource(R.string.about_this_app))
+                                },
+                                onClick = {
+                                    expanded = false
+                                    Toast.makeText(
+                                        context,
+                                        "About This App",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            )
+                        }
+                    }
+                }
             )
         }
     ) { innerPadding->
-        Column(
+        HomeScreenContent(
             modifier = Modifier.padding(innerPadding)
-        ) {
-            Text(
-                text = "Hello Android"
+        )
+    }
+}
+
+@Composable
+fun HomeScreenContent(
+    modifier: Modifier
+){
+    val context = LocalContext.current
+    Column(
+        modifier = modifier.fillMaxSize().padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = stringResource(R.string.products_list),
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
             )
+        ) {
+            Row(
+                modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Absolute.SpaceBetween
+            ) {
+                Column(
+                ) {
+                    Text(
+                        "Aqua 150 ml",
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Medium
+                    )
+                    Text(
+                        "5.000",
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Medium
+                        )
+                    Text(
+                        "Stock: 12",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        Toast.makeText(context, "add to cart", Toast.LENGTH_SHORT).show()
+                    },
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                        contentColor = MaterialTheme.colorScheme.primaryContainer
+                    )
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_add_24),
+                        contentDescription = stringResource(R.string.add_to_cart),
+                        Modifier.size(48.dp)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CheckMyMarketTheme {
+        MainHomeScreen()
     }
 }
